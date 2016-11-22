cdc66d08a210adbdf2f741aee170e19f6b770a1f
Lanlan <liangyuqin1212@gmail.com>
2016-11-23 02:11:43 +0800

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,66 @@
 module.exports = {
-    "extends": "airbnb",
-    "installedESLint": true,
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+    extends: ['airbnb'],
+    plugins: [
+        'react',
+        'jsx-a11y',
+        'import'
+    ],
+    env: {
+        browser: true,
+        jquery: true
+    },
+    rules: {
+        'react/require-extension': 0,
+        'linebreak-style': 0,
+        'prefer-template': 0,
+        'max-len': 0,
+        'no-unused-vars': [2, { vars: 'all', args: 'none', varsIgnorePattern: '^h$' }],
+        'func-names': 0,
+        'prefer-arrow-callback': 0,
+        'one-var': 1,
+        'prefer-const': 1,
+        indent: [2, 4],
+        'no-mixed-operators': 0,
+        'no-alert': 0,
+        'import/no-unresolved': 1,
+        // TODO: delete rule `import/no-extraneous-dependencies` after we get rid of seajs
+        'import/no-extraneous-dependencies': 0,
+        'object-curly-spacing': 0,
+        // other virtual dom libraries are welcomed
+        // "no-unused-vars": [2, { "varsIgnorePattern": "^h$" }],
+        'react/jsx-filename-extension': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/jsx-indent': [2, 4],
+        'jsx-a11y/label-has-for': 0,
+        'jsx-a11y/img-has-alt': 0,
+        'react/prop-types': 1,
+        // deku use class instead of className
+        'react/no-unknown-property': 0,
+        // not now
+        'no-param-reassign': 1,
+
+        'no-confusing-arrow': 0,
+        'arrow-body-style': 0,
+        'no-return-assign': 0,
+
+        // the tailing comma is inconsitent with object spreed syntax: {...a, ...b,} is invalid
+        'comma-dangle': 0,
+        'no-underscore-dangle': 0,
+        'import/no-amd': 0,
+        'no-script-url': 0,
+        'no-new': 0,
+        'wrap-iife': [2, 'inside'],
+        'no-lonely-if': 1
+    },
+    globals: {
+        $: false,
+        require: false,
+        define: false,
+        seajs: false
+    },
+    settings: {
+        'import/resolver': {
+            Handlebars: {}
+        }
+    }
 };
--- requireJS/src/js/com/carousel.js
+++ requireJS/src/js/com/carousel.js
@@ -11,159 +11,157 @@
 *参数配置：
 *第一个参数为容器元素
 *第二个参数可选，为对象，对象内参数为：
-	carouselInterval: 3500,//轮播间隔，单位 ms
-	carouselSpeed: 650,//轮播速度，单位 px
-	isShowArrow: true,//是否显示前后按钮，默认显示为true，可配置false不显示
+carouselInterval: 3500,//轮播间隔，单位 ms
+carouselSpeed: 650,//轮播速度，单位 px
+isShowArrow: true,//是否显示前后按钮，默认显示为true，可配置false不显示
 */
 
 // ---------------------------------------------------------------
 // ---------------------------------------------------------------
 
-define(['jquery'],function($){
-	//$node为传入的轮播容器
-	function Carousel ($node,obj) {
-		//选择ul作为this.$node
-		this.$node = $node.children('#main_carousel .item-ct');
-		this.$items = this.$node.children();
-		this.$arrow = $node.find('.arrow');
-		this.$pre = $node.find('.pre');
-		this.$next = $node.find('.next');
-		this.$bullet = $node.find('.bullet');
-		this.itemWidth = $(window).width();
-		//item数量，方便dom拓展
-		this.itemNum = this.$items.length;
-		//设置状态锁
-	    this.ready = false;
-	    //当前显示的item
-        this.nowView = 0; 
+define(['jquery'], function ($) {
+    // $node为传入的轮播容器
+    function Carousel($node, obj) {
+        // 选择ul作为this.$node
+        this.$node = $node.children('#main_carousel .item-ct');
+        this.$items = this.$node.children();
+        this.$arrow = $node.find('.arrow');
+        this.$pre = $node.find('.pre');
+        this.$next = $node.find('.next');
+        this.$bullet = $node.find('.bullet');
+        this.itemWidth = $(window).width();
+        // item数量，方便dom拓展
+        this.itemNum = this.$items.length;
+        // 设置状态锁
+        this.ready = false;
+        // 当前显示的item
+        this.nowView = 0;
 
         this.init(obj);
-	}
+    }
 
-	Carousel.prototype = {
-		defaultConfig: {
-			carouselInterval: 3000,
-			carouselSpeed: 600,
-			isShowArrow: true
-		},
+    Carousel.prototype = {
+        defaultConfig: {
+            carouselInterval: 3000,
+            carouselSpeed: 600,
+            isShowArrow: true
+        },
 
-		init: function (obj) {
-			this.setObj(obj);
-			this.cloneItem();
-			this.configCSS();
-			this.configUrl(1);
-	 		this.autoPlay();
-	 		this.bindEvent();
-		},
+        init(obj) {
+            this.setObj(obj);
+            this.cloneItem();
+            this.configCSS();
+            this.configUrl(1);
+            this.autoPlay();
+            this.bindEvent();
+        },
 
-		setObj: function (obj) {
-			 this.obj = $.extend({},this.defaultConfig,obj);
-		},
+        setObj(obj) {
+            this.obj = $.extend({}, this.defaultConfig, obj);
+        },
 
-		cloneItem: function () {
-			var _this = this;
-			_this.$node.prepend(_this.$items.last().clone());
-			_this.$node.append(_this.$items.first().clone());	
-			_this.realitemNum = _this.$node.children().length;	 
-		},
+        cloneItem() {
+            const _this = this;
+            _this.$node.prepend(_this.$items.last().clone());
+            _this.$node.append(_this.$items.first().clone());
+            _this.realitemNum = _this.$node.children().length;
+        },
 
-		configCSS: function () {
-			var _this = this;
-			_this.$node.find('li').css('width',_this.itemWidth);
-			_this.$node.find('.bgimg').css('width',_this.itemWidth);	
-			_this.$node.css({
-				left: 0 - _this.itemWidth,
-				width: _this.itemWidth * _this.realitemNum
-			});
-			if (!_this.obj.isShowArrow) {
-				_this.$arrow.css('display','none');
-			}
-		},
+        configCSS() {
+            const _this = this;
+            _this.$node.find('li').css('width', _this.itemWidth);
+            _this.$node.find('.bgimg').css('width', _this.itemWidth);
+            _this.$node.css({
+                left: 0 - _this.itemWidth,
+                width: _this.itemWidth * _this.realitemNum
+            });
+            if (!_this.obj.isShowArrow) {
+                _this.$arrow.css('display', 'none');
+            }
+        },
 
-		configUrl: function (idx) {
-			var $node = this.$node.find('li').eq(idx);
-			var $bgImg = $node.find('.bgimg');
-			$bgImg.css('background-image', 'url('+ $bgImg.attr('bgimg-url') +')');
-		},
+        configUrl(idx) {
+            const $node = this.$node.find('li').eq(idx);
+            const $bgImg = $node.find('.bgimg');
+            $bgImg.css('background-image', 'url(' + $bgImg.attr('bgimg-url') + ')');
+        },
 
-		autoPlay: function () {
-			var _this = this;
-			this.clock = setInterval(function () {
-				_this.playNext(); 
-			}, _this.obj.carouselInterval) 
-		},
+        autoPlay() {
+            const _this = this;
+            this.clock = setInterval(function () {
+                _this.playNext();
+            }, _this.obj.carouselInterval);
+        },
 
-		stopAuto: function () {
-			clearInterval(this.clock);
-		},
+        stopAuto() {
+            clearInterval(this.clock);
+        },
 
-		playPre: function (idx) {
-			var _this = this;
-			var idx = idx || 1;
-			if(!_this.ready){
-				_this.ready = true;
-				_this.configUrl(_this.nowView);
-				_this.$node.animate({ left:'+=' + (idx * _this.itemWidth) },_this.obj.carouselSpeed,'swing',function () {
-					_this.nowView = (_this.itemNum + _this.nowView - idx) % _this.itemNum;
-					if(_this.nowView === (_this.itemNum - 1)){
-						_this.$node.css({left: 0 - _this.itemWidth * _this.itemNum});
-					}
-					_this.bulletActive();
-					_this.ready = false;
-				});
-			}	
-		},
+        playPre(idx) {
+            const _this = this;
+            const myidx = idx || 1;
+            if (!_this.ready) {
+                _this.ready = true;
+                _this.configUrl(_this.nowView);
+                _this.$node.animate({ left: '+=' + (myidx * _this.itemWidth) }, _this.obj.carouselSpeed, 'swing', function () {
+                    _this.nowView = (_this.itemNum + _this.nowView - myidx) % _this.itemNum;
+                    if (_this.nowView === (_this.itemNum - 1)) {
+                        _this.$node.css({ left: 0 - _this.itemWidth * _this.itemNum });
+                    }
+                    _this.bulletActive();
+                    _this.ready = false;
+                });
+            }
+        },
 
-		playNext: function (idx) {
-			var _this = this;
-			var idx = idx || 1;
-			if(!_this.ready){
-				_this.ready = true;
-				_this.configUrl(_this.nowView + 2);
-				_this.$node.animate({ left:'-=' + (idx * _this.itemWidth) },_this.obj.carouselSpeed,'swing',function () {
-					_this.nowView = (_this.nowView + idx) % _this.itemNum;
-					if(_this.nowView === 0){
-						_this.$node.css({left: 0 - _this.itemWidth});
-						_this.nowView = 0;
-					} 
-					_this.bulletActive();
-					_this.ready = false;
-				});		
-			}				 
-		},
+        playNext(idx) {
+            const _this = this;
+            const myidx = idx || 1;
+            if (!_this.ready) {
+                _this.ready = true;
+                _this.configUrl(_this.nowView + 2);
+                _this.$node.animate({ left: '-=' + (myidx * _this.itemWidth) }, _this.obj.carouselSpeed, 'swing', function () {
+                    _this.nowView = (_this.nowView + myidx) % _this.itemNum;
+                    if (_this.nowView === 0) {
+                        _this.$node.css({ left: 0 - _this.itemWidth });
+                        _this.nowView = 0;
+                    }
+                    _this.bulletActive();
+                    _this.ready = false;
+                });
+            }
+        },
 
-		bulletActive: function () {
-			var _this = this;
-			_this.$bullet.children().removeClass("active").eq(_this.nowView).addClass('active')      
-		},
+        bulletActive() {
+            const _this = this;
+            _this.$bullet.children().removeClass('active').eq(_this.nowView).addClass('active');
+        },
 
-		bindEvent: function () {
-			var _this = this;
-			_this.$pre.on('click',function (e) {
-				 e.preventDefault();
-				 _this.playPre();
-			});
-			_this.$next.on('click',function (e) {
-				 e.preventDefault();
-				 _this.playNext();
-			});		
-			_this.$bullet.children().on('click',function () {
-				var idx = $(this).index();
-				if (idx < _this.nowView) {
-					_this.playPre(_this.nowView - idx);
-				}
-				else if (idx > _this.nowView) {
-					_this.playNext(idx - _this.nowView);
-				}
-			}); 
-		}
-	}
+        bindEvent() {
+            const _this = this;
+            _this.$pre.on('click', function (e) {
+                e.preventDefault();
+                _this.playPre();
+            });
+            _this.$next.on('click', function (e) {
+                e.preventDefault();
+                _this.playNext();
+            });
+            _this.$bullet.children().on('click', function () {
+                const idx = $(this).index();
+                if (idx < _this.nowView) {
+                    _this.playPre(_this.nowView - idx);
+                } else if (idx > _this.nowView) {
+                    _this.playNext(idx - _this.nowView);
+                }
+            });
+        }
+    };
 
-	function carousel ($node,obj) {
-		var newCarousel = new Carousel($node,obj);
-		return newCarousel;
-	}
+    function carousel($node, obj) {
+        const newCarousel = new Carousel($node, obj);
+        return newCarousel;
+    }
 
-	return carousel;
-
+    return carousel;
 });
